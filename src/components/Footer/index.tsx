a40528f358e33e06a18da5a0a2a677fdf3d5cb8a
Fanny Dacheux <fanny.dacheux@gmail.com>
2023-02-01 11:28:53 +0100

    feat(footer): improve responsive
    change flex direction and text content depending on the size of the screen

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,26 +1,35 @@
 import style from "./Footer.module.scss";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 function Footer() {
   return (
     <footer className={style.footer}>
       <div className={style.wrapper}>
-        You can find more about my project or about me :
-        <a
-          href="https://github.com/fdacheux"
-          aria-label="Click to see my github account"
-          className={style.footer__link}
-        >
-          <GitHubIcon className={style.socialIcon} />
-        </a>
-        <a
-          href="https://github.com/fdacheux"
-          aria-label="Click to see my linkedin"
-          className={style.footer__link}
-        >
-          <LinkedInIcon className={style.socialIcon} />
-        </a>
+        <div className={style.footer__txt}>
+          <span className={style.widescreensText}>
+            You can find more about my project or about me :
+          </span>
+          <span className={style.smallscreensText}>
+            More about my projects or about me :
+          </span>
+        </div>
+        <div>
+          <a
+            href="https://github.com/fdacheux"
+            aria-label="Click to see my github account"
+            className={style.footer__link}
+          >
+            <GitHubIcon className={style.socialIcon} />
+          </a>
+          <a
+            href="https://github.com/fdacheux"
+            aria-label="Click to see my linkedin"
+            className={style.footer__link}
+          >
+            <LinkedInIcon className={style.socialIcon} />
+          </a>
+        </div>
       </div>
     </footer>
   );
